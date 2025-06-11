792327645450b75a5f4ce957e946847ef04158b1
pedroestrela <169098908+pedroestrele@users.noreply.github.com>
2025-06-11 16:38:47 -0300

    Create PessoaJuridica.java

---
+++ imoveis/PessoaJuridica.java
@@ -0,0 +1,28 @@
+package imoveis;
+
+public class PessoaJuridica extends Responsavel {
+  private String cnpj;
+  private int anoFundacao;
+
+  public PessoaJuridica(String nome, String cnpj, int anoFundacao) {
+      this.nome = nome;
+      this.cnpj = formatar(cnpj);
+      this.anoFundacao = anoFundacao;
+  }
+
+  public String formatar(String cnpj) {
+            // Valida se o CNPJ tem 14 dígitos
+   if (cnpj.length() == 14) {
+            return String.format("%s.%s.%s/%s-%s", cnpj.substring(0, 2), cnpj.substring(2, 5),
+                        cnpj.substring(5, 8), cnpj.substring(8, 12), cnpj.substring(12));
+            } else {
+                return "CNPJ inválido";
+            }
+        }
+  public String getCNPJ(){
+    return this.cnpj;
+  }
+
+public int getAno(){
+  return this.anoFundacao;
+}
