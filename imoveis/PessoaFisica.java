ae5acc50107df348f0aaa2cf95c5998449a3533b
pedroestrela <169098908+pedroestrele@users.noreply.github.com>
2025-06-11 16:46:34 -0300

    Update PessoaFisica.java

--- imoveis/PessoaFisica.java
+++ imoveis/PessoaFisica.java
@@ -7,9 +7,21 @@ public class PessoaFisica extends Responsavel{
     public PessoaFisica(String nome, String cpf, int idade)
     {
         super(nome);
-        this.cpf = cpf;
+        this.cpf = formatar(cpf);
         this.idade = idade;
     }
+
+    @Override
+    public String formatar(String cpf) {
+            // Valida se o CPF tem 11 dígitos
+            if (cpf.length() == 11) {
+                return String.format("%s.%s.%s-%s", cpf.substring(0, 3), cpf.substring(3, 6),
+                        cpf.substring(6, 9), cpf.substring(9));
+            } else {
+                return "CPF inválido";
+            }
+        }
+    
 
 
     public void setNome(String nome) {
